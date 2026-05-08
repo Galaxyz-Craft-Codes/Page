a7caab74e7567a2d8b2f24934e87958736abccc3
lo-kolw <meudrivea48@gmail.com>
2026-05-08 16:09:01 -0300

    Post Version 0.2.1

---
+++ static/js/ref.js
@@ -0,0 +1,18 @@
+const url = window.location
+const refs = new URLSearchParams(url.search)
+
+var blasiverefs = ["Ppauloa37", "Starter", "PBP", "Pombo"]
+
+function brefs() {
+    for (var b = 0; b < blasiverefs.length; b += 1) {
+        if (refs.get("ref") === blasiverefs[b]) {
+
+        }    
+    }
+}
+
+function bh() {
+    
+}
+
+brefs()
